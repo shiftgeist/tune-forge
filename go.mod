6f8d8591bbb591a5d79cac68b1db43f3cd90e989
Felix Hungenberg <30648730+shiftgeist@users.noreply.github.com>
2026-05-09 13:00:22 +0200

    persistant oauth

--- go.mod
+++ go.mod
@@ -3,11 +3,6 @@ module github.com/shiftgeist/tune-forge
 go 1.26.3
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/zmb3/spotify/v2 v2.4.3 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/oauth2 v0.36.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/oauth2 v0.36.0
 )
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -14,10 +16,10 @@ import (
 )
 
 type Client struct {
-	conf     oauth2.Config
-	tok      oauth2.Token
-	verifier string
-	http     http.Client
+	conf        oauth2.Config
+	verifier    string
+	http        http.Client
+	storagePath string
 }
 
 func (c *Client) login(w http.ResponseWriter, r *http.Request) {
@@ -34,13 +36,38 @@ func (c *Client) callback(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 400)
 		return
 	}
-	c.tok = *tok
-	c.http = *c.conf.Client(ctx, &c.tok)
 
+	c.saveSession(*tok)
+	c.http = *oauth2.NewClient(ctx, newTokenSource(c, c.conf.TokenSource(ctx, tok)))
 	fmt.Fprintln(w, "You can close this now")
 }
 
+func (c *Client) saveSession(session oauth2.Token) error {
+	j, err := json.Marshal(session)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(c.storagePath, j, 0644)
+}
+
+func (c *Client) getSession() (*oauth2.Token, error) {
+	byt, err := os.ReadFile(c.storagePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var dat oauth2.Token
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		return nil, err
+	}
+
+	return &dat, nil
+}
+
 func newClient() *Client {
+	ctx := context.Background()
+
 	clientId := strings.TrimSpace(os.Getenv("SPOTIFY_CLIENT_ID"))
 	clientSecret := strings.TrimSpace(os.Getenv("SPOTIFY_CLIENT_SECRET"))
 
@@ -55,7 +82,32 @@ func newClient() *Client {
 		},
 	}
 
-	return &Client{conf: *conf, verifier: oauth2.GenerateVerifier()}
+	c := &Client{conf: *conf, verifier: oauth2.GenerateVerifier(), storagePath: filepath.Join(os.TempDir(), "tune-forge-session.json")}
+
+	persistantTok, err := c.getSession()
+	if err != nil {
+		return c
+	}
+
+	c.http = *oauth2.NewClient(ctx, newTokenSource(c, conf.TokenSource(ctx, persistantTok)))
+	return c
+}
+
+type myTokenSource struct {
+	client *Client
+	inner  oauth2.TokenSource
+}
+
+func newTokenSource(client *Client, inner oauth2.TokenSource) myTokenSource {
+	return myTokenSource{client: client, inner: inner}
+}
+
+func (s myTokenSource) Token() (*oauth2.Token, error) {
+	tok, err := s.inner.Token()
+	if err == nil {
+		s.client.saveSession(*tok)
+	}
+	return tok, err
 }
 
 func main() {
@@ -78,16 +130,21 @@ func helloWorld(c *Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		res, err := c.http.Get("https://api.spotify.com/v1/me")
 		if err != nil {
-			log.Fatal(err)
+			http.Error(w, err.Error(), 502)
+			return
 		}
+		if res.StatusCode > 299 {
+			http.Error(w, "Response failed", res.StatusCode)
+			return
+		}
+
 		body, err := io.ReadAll(res.Body)
 		res.Body.Close()
-		if res.StatusCode > 299 {
-			log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-		}
 		if err != nil {
-			log.Fatal(err)
+			http.Error(w, err.Error(), 422)
+			return
 		}
+
 		fmt.Fprintf(w, "%s", body)
 	}
 }
